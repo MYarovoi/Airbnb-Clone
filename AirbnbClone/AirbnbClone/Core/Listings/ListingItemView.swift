13c872c44e63de0b5c5824cddce49a4da0721ed2
MYarovoi <m.yarovoi96@gmail.com>
2024-10-07 18:05:10 +0300

    Listing Detail View Continued

--- AirbnbClone/AirbnbClone/Core/Listings/ListingItemView.swift
+++ AirbnbClone/AirbnbClone/Core/Listings/ListingItemView.swift
@@ -18,16 +18,9 @@ struct ListingItemView: View {
     var body: some View {
         VStack(spacing: 8) {
             //images
-            TabView {
-                ForEach(images, id: \.self) { image in
-                    Image(image)
-                        .resizable()
-                        .scaledToFill()
-                }
-            }
-            .frame(height: 320)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .tabViewStyle(.page)
+            ListingImageCarouselView()
+                .frame(height: 320)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
             
             //listing details
             HStack(alignment: .top) {
@@ -35,6 +28,7 @@ struct ListingItemView: View {
                 VStack(alignment: .leading) {
                     Text("Miami, Florida")
                         .fontWeight(.semibold)
+                        .foregroundStyle(.black)
                     
                     Text("12 mi away")
                         .foregroundStyle(.gray)
@@ -47,7 +41,7 @@ struct ListingItemView: View {
                             .fontWeight(.semibold)
                         Text("night")
                     }
-                    
+                    .foregroundStyle(.black)
                 }
                 
                 Spacer()
@@ -58,6 +52,7 @@ struct ListingItemView: View {
                     
                     Text("4.82")
                 }
+                .foregroundStyle(.black)
             }
             .font(.footnote)
         }
